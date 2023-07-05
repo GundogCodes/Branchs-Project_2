c7c45818cf605e97d71facd4b24f8f7556fd4ca8
Gunish <gunishsharma20@gmail.com>
2023-07-04 19:27:07 -0600

    part of messages is working

--- controllers/privateMessagesController.js
+++ controllers/privateMessagesController.js
@@ -3,19 +3,66 @@ const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 const pMessages =  require('../models/privateMessages')
 const User = require('../models/user')
+const { findOneAndUpdate } = require('../models/post')
 //privateMessages
 
 exports.sendPrivateMessage = async (req,res) => {
     try {
+        const sendingUser = await User.findOne({'_id':req.user._id})
+        const receivingUser =  await User.findOne({'_id':req.params.id})
+        console.log('sending User', sendingUser)
+        console.log('receiving User',receivingUser)
+        req.body.sender = toString(sendingUser.username)
+        const message = await pMessages.create(req.body)
+        console.log('message: ', message)
+
+        console.log('message text: ',message.text)
+
+        sendingUser.contacts.addToSet(receivingUser.username,receivingUser._id)
+        receivingUser.contacts.addToSet(sendingUser.username,receivingUser._id)
+
+        sendingUser.chats.addToSet(message.text)
+      //  sendingUser.chats.text = `me: ${message.text}`
+
+        receivingUser.chats.addToSet(message.text)
+       // receivingUser.chats.text = `${sendingUser.username}: ${message.text}`
+
+        console.log('receiving User: ',receivingUser)
+        console.log('sending User: ',sendingUser)
+
+        await sendingUser.save()
+        await receivingUser.save()
+
+        res.json(sendingUser.chats)
+        
+    } catch (error) {
+        res.json({message:error.message})
+    }
+}
+
+exports.seeChats  = async (req,res)=>{
+    try {
+        console.log(req.body.id)
+        const foundUserChats = await User.findOne({'_id':req.user._id})
+        res.json(foundUserChats.chats)
+    } catch (error) {
+        res.status(400).json({message:error.message})
+    }
+}
+
+/*
 
         const sendingUser = await User.findOne({'_id':req.user._id})
         const receivingUser =  await User.findOne({'_id':req.params.id})
-        req.body.sender = sendingUser.username
+        console.log('sending User', sendingUser)
+        console.log('receiving User',receivingUser)
+        req.body.sender = toString(sendingUser.username)
         const message = await pMessages.create(req.body)
+
         console.log('message ',message)
 
-        sendingUser.contacts.addToSet(receivingUser.username,receivingUser.id)
-        receivingUser.contacts.addToSet(message.sender,receivingUser.id)
+        sendingUser.contacts.addToSet(receivingUser.username,receivingUser._id)
+        receivingUser.contacts.addToSet(message.sender,receivingUser._id)
 
         sendingUser.chats.sender = receivingUser.username
         sendingUser.chats.text = `me: ${message.text}`
@@ -30,18 +77,5 @@ exports.sendPrivateMessage = async (req,res) => {
         await receivingUser.save()
 
         res.json(sendingUser.chats)
-
-    } catch (error) {
-        res.json({message:error.message})
-    }
-}
-
-exports.seeChats  = async (req,res)=>{
-    try {
-        console.log(req.body.id)
-        const foundUserChats = await User.findOne({'_id':req.user.id})
-        res.json(foundUserChats.chats)
-    } catch (error) {
-        res.status(400).json({message:error.message})
-    }
-}
+        
+*/
