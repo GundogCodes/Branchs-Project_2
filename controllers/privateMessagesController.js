616eab30dc2d5c4e3d1e0a2f0018e0d5176ee98b
Gunish <gunishsharma20@gmail.com>
2023-07-08 00:38:27 -0600

    presentation ready version

--- controllers/privateMessagesController.js
+++ controllers/privateMessagesController.js
@@ -82,33 +82,3 @@ exports.seeChats  = async (req,res)=>{
         res.status(400).json({message:error.message})
     }
 }
-
-/*
-
-        const sendingUser = await User.findOne({'_id':req.user._id})
-        const receivingUser =  await User.findOne({'_id':req.params.id})
-        console.log('sending User', sendingUser)
-        console.log('receiving User',receivingUser)
-        req.body.sender = toString(sendingUser.username)
-        const message = await pMessages.create(req.body)
-
-        console.log('message ',message)
-
-        sendingUser.contacts.addToSet(receivingUser.username,receivingUser._id)
-        receivingUser.contacts.addToSet(message.sender,receivingUser._id)
-
-        sendingUser.chats.sender = receivingUser.username
-        sendingUser.chats.text = `me: ${message.text}`
-
-        receivingUser.chats.sender = sendingUser.username
-        receivingUser.chats.text = `${sendingUser.username}: ${message.text}`
-
-        console.log('receiving User: ',receivingUser)
-        console.log('sending User: ',sendingUser)
-
-        await sendingUser.save()
-        await receivingUser.save()
-
-        res.json(sendingUser.chats)
-        
-*/
